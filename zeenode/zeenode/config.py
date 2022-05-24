2621504b67ce68d85f3b5d695ea67eae381c71c6
VasilijusZabauskas <106160405+VasilijusZabauskas@users.noreply.github.com>
2022-05-24 17:54:54 +0300

    Update config.py

--- zeenode/zeenode/config.py
+++ zeenode/zeenode/config.py
@@ -1,5 +1,5 @@
 class config:
-    global auth; auth = "" # Enter your discord token for Auto-Login.
-    global prefix; prefix = "$" # Enter your prefix for selfbot.
+    global auth; auth = "ODk2NDQyMDkzNjg0MjY1MDEx.GOLRce.YVNe_hGrYR5D3YWbMy8Xdg49o8bVmKtlrpWcII" # Enter your discord token for Auto-Login.
+    global prefix; prefix = "-" # Enter your prefix for selfbot.
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
     global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
